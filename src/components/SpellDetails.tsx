29977b945e2e5593b623cec16224af25117e2b40
ameeriit <amritpokharel51@gmail.com>
2024-05-12 01:16:01 +0545

    refactor: spelldetails component

--- src/components/SpellDetails.tsx
+++ src/components/SpellDetails.tsx
@@ -1,5 +1,4 @@
-import ModalFavBtn from "./Buttons/ModalFavBtn";
-
+import ModalFavBtn from "@/components/Buttons/ModalFavBtn";
 import Modal from "@/components/Modal";
 import { SpellDetail } from "@/types/spell";
 
@@ -7,62 +6,41 @@ interface SpellDetailsProps {
   spellDetail: SpellDetail;
 }
 
-export default async function SpellDetails({ spellDetail }: SpellDetailsProps) {
+export default function SpellDetails({ spellDetail }: SpellDetailsProps) {
   return (
     <Modal>
       <div className="flex flex-wrap gap-4">
         <div className="flex justify-between w-full items-center">
           <h5>
-            <span className="font-sedgwick  text-3xl">Name:</span>{" "}
+            <span className="font-sedgwick text-3xl">Name:</span>{" "}
             {spellDetail?.name}
           </h5>
           <ModalFavBtn spellDetail={spellDetail} />
         </div>
         <div className="flex flex-col">
-          {spellDetail?.range && (
-            <h6>
-              <span className="headerSpan">Range:</span> {spellDetail.range}
-            </h6>
+          {renderSpellDetail("Range:", spellDetail?.range)}
+          {renderSpellDetail("Material:", spellDetail?.material)}
+          {renderSpellDetail("Duration:", spellDetail?.duration)}
+          {renderSpellDetail("Casting Time:", spellDetail?.casting_time)}
+          {renderSpellDetail("Attack Type:", spellDetail?.attack_type)}
+          {renderSpellDetail(
+            "Damage Type:",
+            spellDetail?.damage?.damage_type.name
           )}
-          {spellDetail?.material && (
-            <h6>
-              <span className="headerSpan">Material:</span>{" "}
-              {spellDetail.material}
-            </h6>
-          )}
-          {spellDetail?.duration && (
-            <h6>
-              <span className="headerSpan">Duration:</span>{" "}
-              {spellDetail.duration}
-            </h6>
-          )}
-          {spellDetail?.casting_time && (
-            <h6>
-              <span className="headerSpan">Casting Time::</span>{" "}
-              {spellDetail.casting_time}
-            </h6>
-          )}
-          {spellDetail?.attack_type && (
-            <h6>
-              <span className="headerSpan">Attack Type:</span>{" "}
-              {spellDetail.attack_type}
-            </h6>
-          )}
-          {spellDetail?.damage?.damage_type.name && (
-            <h6>
-              <span className="headerSpan">Damage Type:</span>{" "}
-              {spellDetail.damage.damage_type.name}
-            </h6>
-          )}
-          {spellDetail?.school.name && (
-            <h6>
-              <span className="headerSpan">School:</span>{" "}
-              {spellDetail.school.name}
-            </h6>
-          )}
+          {renderSpellDetail("School:", spellDetail?.school.name)}
         </div>
         <p className="paragraph">{spellDetail?.desc}</p>
       </div>
     </Modal>
+  );
+}
+
+function renderSpellDetail(title: string, description?: string) {
+  return (
+    description && (
+      <h6>
+        <span className="headerSpan">{title}</span> {description}
+      </h6>
+    )
   );
 }
